6eaac3e9df16daf98e6b39a4ac5eb5c84b1f1798
Merline Belizaire <merline@noukod.com>
2018-09-03 15:18:00 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,5 @@
 # code your #position_taken? method here!
+puts " #{board[col1]} | #{board[col2]} | #{board[col3]} "
 def position_taken?(board,index)
 
 if board[index] == " " || board[index] == "" || board[index] == nil
